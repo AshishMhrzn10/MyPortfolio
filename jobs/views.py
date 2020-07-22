648c814707f8a97cd36b799ad857829bc9dafb55
Ashish Maharjan <amaharjan1032@gmail.com>
2020-07-22 20:39:26 +0545

    views page

--- jobs/views.py
+++ jobs/views.py
@@ -4,7 +4,7 @@ from .models import *
 # Create your views here.
 
 def home(request):
-    about = About.objects.all().get()
+    about = About.objects.get()
     skills = Skill.objects.all().order_by('id')
     works = RecentWork.objects.all()
     return render(request,'home.html',{'about':about,'skills':skills,'works':works})
